bac4baae16fbd3bf4a78cacff0c3ef1fa56f970b
Jarren Bess <aarzos@users.noreply.github.com>
2024-06-30 20:40:31 -0400

    feat: add function to see who wins the round.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -24,3 +24,29 @@ function getHumanChoice(){
         alert(`${userInput} is not a valid input Selection. Please try again.`);
     }
 }
+
+function playRound(humanChoice, computerChoice){
+    let humanSelction = humanChoice.toLowerCase();
+
+    if(humanSelction == "rock" && computerChoice == "scissors"){
+        humanScore++;
+        console.log("You win! Rock beats scissors.");
+    } else if(humanSelction == "paper" && computerChoice == "rock"){
+        humanScore++;
+        console.log("You win! Paper beats rock.");
+    } else if(humanSelction == "scissors" && computerChoice == "paper"){
+        humanScore++;
+        console.log("You win! Scissors beats paper.");
+    } else if(humanSelction == "paper" && computerChoice == "scissors"){
+        computerScore++;
+        console.log("You lose! Scissors beats paper.");
+    } else if(humanSelction == "scissors" && computerChoice == "rock"){
+        computerScore++;
+        console.log("You lose! Rock beats scissors.");
+    } else if(humanSelction == "rock" && computerChoice == "paper"){
+        computerScore++;
+        console.log("You lose! Paper beats rock.");
+    } else {
+        console.log("It is a tie.");
+    }
+}
